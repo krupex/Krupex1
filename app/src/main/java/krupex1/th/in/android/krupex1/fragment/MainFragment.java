d6fb495f82ca2991b77ed9aac61a6d0710d9d0da
krupex <chitsanu2011@gmail.com>
2018-04-25 16:28:25 +0700

    Replate fragmet sucsess

--- app/src/main/java/krupex1/th/in/android/krupex1/fragment/MainFragment.java
+++ app/src/main/java/krupex1/th/in/android/krupex1/fragment/MainFragment.java
@@ -7,11 +7,34 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import krupex1.th.in.android.krupex1.R;
 
 public class MainFragment extends Fragment{
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // register control
+
+
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Replat frgament
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contenMainFragment, new RegisterFlagment())
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+
+    }  // main metod
 
     @Nullable
     @Override
---
+++ app/src/main/java/krupex1/th/in/android/krupex1/fragment/RegisterFlagment.java
@@ -0,0 +1,20 @@
+package krupex1.th.in.android.krupex1.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import krupex1.th.in.android.krupex1.R;
+
+public class RegisterFlagment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.flagment_register, container, false);
+        return view;
+    }
+}
