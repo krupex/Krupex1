1aa3d93e1098a4c52697554369cf8f9bb095d2f9
krupex <chitsanu2011@gmail.com>
2018-04-25 17:10:11 +0700

    end 25042018

--- app/src/main/java/krupex1/th/in/android/krupex1/fragment/RegisterFlagment.java
+++ app/src/main/java/krupex1/th/in/android/krupex1/fragment/RegisterFlagment.java
@@ -4,13 +4,41 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import krupex1.th.in.android.krupex1.MainActivity;
 import krupex1.th.in.android.krupex1.R;
 
-public class RegisterFlagment extends Fragment{
+public class RegisterFlagment extends Fragment {
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+//        crete bar
+        creatTtoolbar();
+
+        // main medtod
+    }
+
+    private void creatTtoolbar() {
+        Toolbar toolbar = getActivity().findViewById(R.id.toolbarRegister);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+//        setup tital
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("please Fill All Blank");
+
+//        setup navi icon
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+    }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
