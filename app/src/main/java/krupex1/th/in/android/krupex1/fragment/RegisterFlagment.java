2cd9024db29d7b3f98d48beb1fa5570e53be52c7
krupex <chitsanu2011@gmail.com>
2018-04-26 12:36:13 +0700

    start2 26042018

--- app/src/main/java/krupex1/th/in/android/krupex1/fragment/RegisterFlagment.java
+++ app/src/main/java/krupex1/th/in/android/krupex1/fragment/RegisterFlagment.java
@@ -6,11 +6,16 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import krupex1.th.in.android.krupex1.MainActivity;
 import krupex1.th.in.android.krupex1.R;
+import krupex1.th.in.android.krupex1.utility.MyAlert;
 
 public class RegisterFlagment extends Fragment {
     @Override
@@ -20,6 +25,59 @@ public class RegisterFlagment extends Fragment {
         creatTtoolbar();
 
         // main medtod
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.itemUpload ){
+
+            uploadValueToServer();
+
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+//    upload to server
+
+    private void uploadValueToServer() {
+
+//        get value frome edit text
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText userEditText = getView().findViewById(R.id.edtUser);
+        EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+//                Change Data type from edit text to sting
+        String nameString = nameEditText.getText().toString().trim();
+        String userString = userEditText.getText().toString().trim();
+        String passwordString = passwordEditText.getText().toString().trim();
+
+
+//        Check space
+
+        if (nameString.isEmpty() ||  userString.isEmpty()|| passwordString.isEmpty()) {
+            MyAlert myAlert = new MyAlert(getActivity());
+            myAlert.normalDialog("Have space", "plase fill all blank");
+//            have space
+        } else {
+        }
+
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_register, menu);
+
+
     }
 
     private void creatTtoolbar() {
@@ -37,8 +95,12 @@ public class RegisterFlagment extends Fragment {
             public void onClick(View v) {
                 getActivity().getSupportFragmentManager().popBackStack();
             }
+
         });
+        setHasOptionsMenu(true);
+
     }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
---
+++ app/src/main/java/krupex1/th/in/android/krupex1/utility/Myconstant.java
@@ -0,0 +1,10 @@
+package krupex1.th.in.android.krupex1.utility;
+
+public class Myconstant {
+
+    private String urlAddUser = "http://androidthai.in.th/bru/addUser.php";
+
+    public String getUrlAddUser() {
+        return urlAddUser;
+    }
+}
